e1b89392253a6e2b09763c3197bcd7597fa556e4
GitHub Action <action@github.com>
2022-03-16 02:21:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/wesbin/intellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wesbin/intellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wesbin.intellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wesbin.intellijplugin.MyBundle
 
 class MyApplicationService {
 
