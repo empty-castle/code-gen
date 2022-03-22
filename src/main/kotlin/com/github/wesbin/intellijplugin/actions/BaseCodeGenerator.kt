34341b5b77ceefe02cd11f01d277db9324917143
wesbin <leegosoo11@naver.com>
2022-03-22 18:00:39 +0900

    column 찾았다

--- src/main/kotlin/com/github/wesbin/intellijplugin/actions/BaseCodeGenerator.kt
+++ src/main/kotlin/com/github/wesbin/intellijplugin/actions/BaseCodeGenerator.kt
@@ -1,26 +1,33 @@
 package com.github.wesbin.intellijplugin.actions
 
+import com.intellij.database.model.DasColumn
 import com.intellij.database.psi.DbTable
+import com.intellij.database.util.DasUtil
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.ui.Messages
 import com.intellij.psi.PsiElement
+import com.intellij.util.containers.JBIterable
+import com.intellij.util.containers.JBIterator
 
 class BaseCodeGenerator: DumbAwareAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.getData(PlatformDataKeys.PROJECT)
 
-        var psiElement : PsiElement? = CommonDataKeys.PSI_ELEMENT.getData(e.dataContext)
+        val psiElement : PsiElement? = CommonDataKeys.PSI_ELEMENT.getData(e.dataContext)
+        for (dasColumn in DasUtil.getColumns(psiElement as DbTable)) {
+            println(dasColumn.name)
+        }
 
-//        DasUtil.getColumns(psiElement as DbTable).get(1).name
-
-//        if (psiElement != null) {
-//            psiElement
+//        while (dasColumns.hasNext()) {
+//            val dasColumn = dasColumns.next()
+//            println(dasColumn.name)
 //        }
 
+//        DasUtil.getColumns(psiElement as DbTable).get(1).name
         //psiElement 는 우클릭한 테이블을 바라보네
 
 //        DbTableImpl
