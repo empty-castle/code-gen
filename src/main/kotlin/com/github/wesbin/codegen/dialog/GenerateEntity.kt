b1925b1686e13958d54efd7489227e3a9c0d886c
wesbin <leegosoo11@naver.com>
2022-07-20 17:02:05 +0900

    GenerateEntity.kt init

--- src/main/kotlin/com/github/wesbin/codegen/dialog/GenerateEntity.kt
+++ src/main/kotlin/com/github/wesbin/codegen/dialog/GenerateEntity.kt
@@ -1,4 +1,32 @@
 package com.github.wesbin.codegen.dialog
 
-class GenerateEntity {
+import com.intellij.lang.Language
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.guessProjectDir
+import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiFileFactory
+import com.intellij.psi.PsiManager
+
+class GenerateEntity(val project: Project) {
+
+    fun create() {
+        ApplicationManager.getApplication().runWriteAction {
+            val psiFile: PsiFile? = Language.findLanguageByID("kotlin")?.let {
+                 PsiFileFactory.getInstance(project)
+                    .createFileFromText("TEST.kt", it, "entity create test")
+            }
+
+            val findDirectory: PsiDirectory? = project.guessProjectDir()?.let {
+                PsiManager.getInstance(project).findDirectory(it)
+            }
+
+            if (findDirectory != null) {
+                psiFile?.let {
+                    findDirectory.add(it)
+                }
+            }
+        }
+    }
 }
