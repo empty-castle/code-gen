1810a0832f1497a67da94093bc89caba35dd1a9c
wesbin <leegosoo11@naver.com>
2022-08-02 11:38:01 +0900

    CodeGen.kt genEnitity ING #3

--- src/main/kotlin/com/github/wesbin/codegen/dialog/CodeGen.kt
+++ src/main/kotlin/com/github/wesbin/codegen/dialog/CodeGen.kt
@@ -1,6 +1,7 @@
 package com.github.wesbin.codegen.dialog
 
 import com.github.wesbin.codegen.dialog.panel.ObservableProperties
+import com.github.wesbin.codegen.dialog.util.StringUtil
 import com.github.wesbin.codegen.dialog.util.TypeUtil
 import com.intellij.database.model.DasColumn
 import com.intellij.database.util.DasUtil
@@ -11,28 +12,31 @@ object CodeGen {
     fun genEntity(observableProperties: ObservableProperties): String {
 
         val packageName: String = observableProperties.selectedSourceRoot?.text ?: "com.wesbin.codegen"
-        var result = """
-            |package $packageName
-            |
-        """.trimMargin()
-
         val imports: MutableList<String> = mutableListOf<String>()
-        val fields: MutableList<String> = mutableListOf<String>()
+        val fields: MutableList<Pair<String, String>> = mutableListOf<Pair<String, String>>()
 
         DasUtil.getColumns(observableProperties.selectedTable).forEach { dasColumn: DasColumn? ->
             if (dasColumn != null) {
                 val attributeType = TypeUtil.toAttributeType(dasColumn.dataType)
                 val importAttributeType = TypeUtil.toImportAttributeType(attributeType)
                 importAttributeType?.let(imports::add)
-                fields.add(attributeType)
+                fields.add(Pair(attributeType, StringUtil.toCamelCase(dasColumn.name)))
             }
         }
 
+        // start entity
+        var result = """
+            |package $packageName
+            |
+        """.trimMargin()
+
+        // set import
         imports.forEach { s: String -> result += """
             |
             |import $s
         """.trimMargin() }
 
+        // start class
         result += """
             |
             |
@@ -40,14 +44,16 @@ object CodeGen {
             |
         """.trimMargin()
 
-        fields.forEach { s: String ->
+        // set field
+        fields.forEach { (attributeType, attributeName) ->
             result += """
                 |
-                |   private $s
+                |   private $attributeType $attributeName;
                 |
             """.trimMargin()
         }
 
+        // end class
         result += """
             |
             |}
