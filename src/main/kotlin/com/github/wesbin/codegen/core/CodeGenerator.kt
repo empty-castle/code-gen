a493f1023820400441a8c4e7064a669dcb5347bb
wesbin <leegosoo11@naver.com>
2022-08-19 11:16:58 +0900

    unuse import remove

--- src/main/kotlin/com/github/wesbin/codegen/core/CodeGenerator.kt
+++ src/main/kotlin/com/github/wesbin/codegen/core/CodeGenerator.kt
@@ -1,11 +1,6 @@
 package com.github.wesbin.codegen.core
 
-import com.github.wesbin.codegen.core.entity.EntityField
-import com.github.wesbin.codegen.core.entity.EntityFieldFactory
 import com.github.wesbin.codegen.dialog.panel.ObservableProperties
-import com.github.wesbin.codegen.util.TypeUtil
-import com.intellij.database.model.DasColumn
-import com.intellij.database.util.DasUtil
 
 class CodeGenerator(actionId: String) {
 
