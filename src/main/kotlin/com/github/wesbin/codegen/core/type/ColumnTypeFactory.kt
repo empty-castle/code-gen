6e3c2e7541d1847de44ae6074495ff9df501b171
wesbin <leegosoo11@naver.com>
2022-08-26 15:19:45 +0900

    idea update

---
+++ src/main/kotlin/com/github/wesbin/codegen/core/type/ColumnTypeFactory.kt
@@ -0,0 +1,9 @@
+package com.github.wesbin.codegen.core.type
+
+class ColumnTypeFactory {
+    /*
+    * todo
+    * actionId 를 따라서 군데군데에서 쓰는게 아니라 한 군데에서 module, type 등등을 일괄로 생성 돌려쓰기
+    *
+    * */
+}
