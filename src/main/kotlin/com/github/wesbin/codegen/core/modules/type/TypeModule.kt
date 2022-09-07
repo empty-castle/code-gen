f742cfb1d9a33ffa7492fc994a3f825db1762c81
wesbin <leegosoo11@naver.com>
2022-09-07 11:44:31 +0900

    Type Mapping 참조 URL 추가

--- src/main/kotlin/com/github/wesbin/codegen/core/modules/type/TypeModule.kt
+++ src/main/kotlin/com/github/wesbin/codegen/core/modules/type/TypeModule.kt
@@ -3,6 +3,9 @@ package com.github.wesbin.codegen.core.modules.type
 import com.github.wesbin.codegen.core.modules.type.kind.*
 import com.intellij.database.model.DataType
 
+/*
+* https://documentation.softwareag.com/webmethods/adapters_estandards/Adapters/JDBC/JDBC_10-3/10-3_Adapter_for_JDBC_webhelp/index.html#page/jdbc-webhelp/co-jdbc_data_type_to_java_data_type.html
+* */
 class TypeModule private constructor() {
 
     companion object {
